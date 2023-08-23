5ae857d3e7c12108fcaff27d6968dde98ff8cb68
Andrea Creazzi <andreacreazzi@libero.it>
2023-08-23 14:48:34 +0200

    add db Seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -17,6 +17,16 @@ class TrainSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $newTrain = new Train();
+            $newTrain->agencies = $faker->company();
+            $newTrain->stations_departures = $faker->city();
+            $newTrain->stations_arrivals = $faker->city();
+            $newTrain->times_departures = $faker->time();
+            $newTrain->times_arrivals = $faker->time();
+            $newTrain->trains_code = $faker->randomNumber(5, true);
+            $newTrain->numbers_carriages = $faker->randomNumber(2, true);
+            $newTrain->in_time = $faker->boolean();
+            $newTrain->cancelled = $faker->boolean();
+            $newTrain->save();
         }
     }
 }
